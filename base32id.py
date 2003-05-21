22adb47d0d6a4ffd0de59d41b96746b60a680882
zooko <zooko>
2003-05-21 02:24:03 +0000

    * base32id updates humanreadable.hr() when base32id is imported

--- base32id.py
+++ base32id.py
@@ -1,22 +1,24 @@
-#  Copyright (c) 2002 Bryce "Zooko" Wilcox-O'Hearn
+#  Copyright (c) 2002, 2003 Bryce "Zooko" Wilcox-O'Hearn
 #  portions Copyright (c) 2001 Autonomous Zone Industries
 #  This file is licensed under the
 #    GNU Lesser General Public License v2.1.
 #    See the file COPYING or visit http://www.gnu.org/ for details.
 
-__revision__ = "$Id: base32id.py,v 1.3 2003/04/03 22:30:35 zooko Exp $"
+__version__ = "$Revision: 1.4 $"
+# $Source: /home/zooko/playground/libbase32/rescue-party/gw/../libbase32/libbase32/base32id.py,v $
 
-# Python Standard Library modules
+True = 1 == 1
+False = not True
+
 import string
 
-# pyutil modules # http://sf.net/projects/pyutil # check out the CVS module named "pyutil_new"
-from pyutil.humanreadable import BetterRepr
+from pyutil import humanreadable
 
-# base32 modules
 import base32 
 
 printableascii = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_=+!@#$%^&*()`~[]\{}|;':\",./<>? \t" # I just typed this in by looking at my keyboard.  It probably doesn't matter much if I missed some, because I only use it to guess whether a 20-byte string should be represented as a string or as an ID.  If all of the characters in the string are found `printableascii', then we guess that it is a string, not an id.
 nulltrans = string.maketrans('', '')
+
 
 def abbrev(id, b2a_l=base32.b2a_l, trimnpad=base32.trimnpad):
     return '<' + b2a_l(trimnpad(id[:4], 25), 25) + '>'
@@ -30,14 +32,14 @@ def could_be_id(id, could_be_base32_encoded_l=base32.could_be_base32_encoded_l):
 def could_be_abbrev(id, could_be_base32_encoded_l=base32.could_be_base32_encoded_l):
     return could_be_base32_encoded_l(id, 25)
 
-class AbbrevRepr(BetterRepr):
+class AbbrevRepr(humanreadable.BetterRepr):
     def __init__(self):
-        BetterRepr.__init__(self)
+        humanreadable.BetterRepr.__init__(self)
 
-    def repr_str(self, obj, level, could_be_base32_encoded_l=base32.could_be_base32_encoded_l, translate=string.translate, nulltrans=nulltrans, printableascii=printableascii, abbrev=abbrev):
+    def repr_str(self, obj, level, could_be_base32_encoded_l=base32.could_be_base32_encoded_l, nulltrans=nulltrans, printableascii=printableascii, abbrev=abbrev):
         if len(obj) == 20:
             # But maybe it was just a 20-character human-readable string, like "credit limit reached", so this is an attempt to detect that case.
-            if len(translate(obj, nulltrans, printableascii)) == 0:
+            if len(obj.translate(nulltrans, printableascii)) == 0:
                 if self.maxstring >= 22:
                     return `obj`
 
@@ -65,3 +67,7 @@ class AbbrevRepr(BetterRepr):
                 s = s[:i] + '...' + s[len(s)-j:]
             # ... done inlining `repr.repr_string()'
             return s
+
+# use base32id repr to pretty-print ids embedded in structures
+humanreadable.brepr = AbbrevRepr()
+
